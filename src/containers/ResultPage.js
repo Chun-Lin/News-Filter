711fa42d1549c093f5cfe409d14011d97b97a2c4
Chun-Lin <wulin40063@gmail.com>
2018-06-13 15:14:54 +0800

    style: add tag component to the ResultPage

--- src/containers/ResultPage.js
+++ src/containers/ResultPage.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
 import ResultLayout from '../components/ResultPage/ResultLayout'
 import ResultTitle from '../components/ResultPage/ResultTitle'
+import Tag from '../components/ResultPage/Tag'
+
+const TagWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+`
 
 const ResultPage = () => {
   return (
     <ResultLayout>
       <ResultTitle resultCount={15} />
+      <TagWrapper>
+        <Tag>Business</Tag>
+        <Tag>Entertainment</Tag>
+        <Tag>Business</Tag>
+      </TagWrapper>
     </ResultLayout>
   )
 }
