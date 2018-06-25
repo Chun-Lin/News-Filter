dfc26764bb0326a54bedf60e3f38914c2a56e401
Chun-Lin <wulin40063@gmail.com>
2018-06-25 15:43:50 +0800

    fix: remove useless import in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import Header from './components/Header/Header'
-import Location from './components/Sidebar/Location'
-import Date from './components/Sidebar/Date'
+import Header from './containers/Header'
 import Layout from './containers/Layout'
-import Category from './components/Sidebar/Category'
 import Sidebar from './containers/Sidebar'
 import ResultPage from './containers/ResultPage'
 import 'nodep-date-input-polyfill'
-import Header from './containers/Header'
 
 class App extends Component {
   render() {
